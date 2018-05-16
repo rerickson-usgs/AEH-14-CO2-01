35a6c985d3c6ffce738786312d05937a95c747b9
rerickson <rerickson@usgs.gov>
2018-05-16 11:28:50 -0500

    added more analysis

--- Analysis.R
+++ Analysis.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(data.table)
 library(scales)
-library(lme4)
 library(drc)
+library(lme4)
 
-
+###########################################################################################################
+###########################################################################################################
 ## Load files
 file_path_4C = "../4C Toxicity Data for review/csv files/"
 unionid_4C <- fread(paste0(file_path_4C,
@@ -26,11 +27,14 @@ ZBM_lw_20C <- fread(paste0(file_path_20C,
                           "ZBM weights and length 20 C trial for review.csv"), na.strings = ".")
 
 zebra_mussel_20C <- fread(paste0(file_path_20C,
-                                "/Zebra Mussel assessment 20C trial for review.csv"), na.strings = ".")
+                                 "/Zebra Mussel assessment 20C trial for review.csv"), na.strings = ".")
 
-## Check to make sure data formatted the same
-## Fix what is not by renaming or deleting phantom columns (N.B., ":= NULL" is how data.table deletes columns)
-## Add in temp as a new column 
+###########################################################################################################
+###########################################################################################################
+## Check and Format files 
+## Fix what is not by renaming or deleting phantom columns
+## (N.B., ":= NULL" is how data.table deletes columns)
+## Add in Temp as a new column 
 ## and then merge together across trials 
 
 colnames(unionid_20C)
@@ -61,12 +65,14 @@ colnames(zebra_mussel_4C)
 zebra_mussel_4C[ , c('V17', 'V18', 'V19') := NULL]
 colnames(zebra_mussel_20C) == colnames(zebra_mussel_4C)
 
-zebra_mussel_20C[ , temp := 20]
-zebra_mussel_4C[ , temp := 4]
+zebra_mussel_20C[ , Temp := 20]
+zebra_mussel_4C[ , Temp := 4]
 
 zebra_mussel <- rbind(zebra_mussel_20C, zebra_mussel_4C)
 
-## Examine survival unionid data
+###########################################################################################################
+###########################################################################################################
+## Examine unionid survival data
 summary(unionid)
 unionid[ , Tank := factor(Tank)]
 
@@ -88,11 +94,6 @@ ggsave("uninidPlotSurvival.pdf", unionidPlot)
 unionid[ , pCO2 := mpCO2 / 1000.0]
 summary(unionid[ , .(pCO2, mpCO2, Tank)])
 
-## Run GLMER on results
-unionidGlmerOut <- glmer( Alive ~ pCO2 + (1| Tank), unionid[ TimeCode == 7 & Temp == 20, ], family = 'binomial')
-summary(unionidGlmerOut)
-
-
 unoidByTank <- unionid[ , .(Alive = sum(Alive, na.rm = TRUE),
                             Byssus = sum(Byssus, na.rm = TRUE),
                             pCO2 = mean(pCO2), mpCO2 = mean(mpCO2),
@@ -111,7 +112,7 @@ unoidDRC <- drm(pAlive ~ pCO2, weights = N, data = unoidByTank[ TimeCode == 7 & 
 unoidDRC
 
 plot(unoidDRC)
-ED(unoidDRC, respLev = c(0.5, 0.01, 0.99), interval = 'delta')
+ED(unoidDRC, respLev = c( 50, 1, 99), interval = 'delta')
 
 
 ## look at unionid Byssus
@@ -132,10 +133,9 @@ unionidPlotByssus
 ggsave("uninidPlotByssus.pdf", unionidPlotByssus)
 
 ## Fit dose-response curve and estiamte LC end points 
-unoidDRC_byssus_7_20 <- drm(pByssus ~ pCO2, weights = N,
-                            data = unoidByTank[ TimeCode == 7 & Temp == 20, ],
-                            fct = LL.2(), type = 'binomial')
-unoidDRC_byssus_7_20
+## unoidDRC_byssus_7_20 <- drm(pByssus ~ pCO2, weights = N,
+##                             data = unoidByTank[ TimeCode == 7 & Temp == 20, ],
+##                             fct = LL.2(), type = 'binomial')
 
 unoidDRC_byssus_96_20 <- drm(pByssus ~ pCO2, weights = N,
                             data = unoidByTank[ TimeCode == 96 & Temp == 20, ],
@@ -143,7 +143,7 @@ unoidDRC_byssus_96_20 <- drm(pByssus ~ pCO2, weights = N,
 unoidDRC_byssus_96_20
 
 plot(unoidDRC_byssus_96_20)
-ED(unoidDRC_byssus_96_20, respLev = c(0.5, 0.01, 0.99), interval = 'delta')
+ED(unoidDRC_byssus_96_20, respLev = c( 50, 1, 99), interval = 'delta')
 
 
 unoidDRC_byssus_96_4 <- drm(pByssus ~ pCO2, weights = N,
@@ -152,7 +152,7 @@ unoidDRC_byssus_96_4 <- drm(pByssus ~ pCO2, weights = N,
 unoidDRC_byssus_96_4
 
 plot(unoidDRC_byssus_96_4)
-ED(unoidDRC_byssus_96_4, respLev = c(0.5, 0.01, 0.99), interval = 'delta')
+ED(unoidDRC_byssus_96_4, respLev = c( 50, 1, 99), interval = 'delta')
 
 
 unoidDRC_byssus_7_4 <- drm(pByssus ~ pCO2, weights = N,
@@ -161,7 +161,148 @@ unoidDRC_byssus_7_4 <- drm(pByssus ~ pCO2, weights = N,
 unoidDRC_byssus_7_4
 
 plot(unoidDRC_byssus_7_4)
-ED(unoidDRC_byssus_7_4, respLev = c(0.5, 0.01, 0.99), interval = 'delta')
+## ED(unoidDRC_byssus_7_4, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+###########################################################################################################
+###########################################################################################################
+## Analyze Zebra Mussel length and weight data
+setnames(ZBM_lw, gsub(" |\\(|\\)", "", colnames(ZBM_lw)))
+ZBM_lw[ , TempFactor := factor(Temp)]
+
+ggplot(ZBM_lw, aes(x = ExpDurh, y = Lengthmm, fill = TrtGroup)) + geom_boxplot() +
+    facet_grid( Size ~ Temp, scale = "free_y", labeller = label_bquote(cols = "Temperature"~ .(Temp) *degree*"C")) +
+    scale_fill_manual(values = c("blue", "red", "orange")) + 
+    theme_minimal() +
+    ylab("Length (mm)") +
+    xlab("Exposure duration")
+
+summary(lm(Lengthmm ~ TrtGroup* Size + Temp, data = ZBM_lw[  ExpDurh == '72h', ]))
+
+###########################################################################################################
+###########################################################################################################
+## Analyze Zebra Mussel mortality data
+setnames(zebra_mussel, "ExpDur(h)", "ExpDur")
+
+zebra_mussel[ , pCO2 := mpCO2/ 1000 ]
+
+zebra_mussel[ , pAlive := Alive / Total ]
+
+zebra_mussel
+
+zmAlivePlot <-
+    ggplot(data = zebra_mussel[ TimeCode == 7], aes(x = mpCO2, y = pAlive, weight = Total)) +
+    geom_jitter(width = 0, height = 0.05) +
+    facet_grid( Size ~ Temp,
+               labeller = label_bquote(
+                   rows = "Size: "~.(Size),
+                   cols = .(Temp)*degree*"C"),
+               scales = "free_x") +
+    theme_minimal() +
+    xlab(expression("mP"~CO[2])) + 
+    scale_x_continuous(label = comma, trans = 'log10')  +    
+    stat_smooth(method = 'glm', method.args = list(family = 'binomial'))
+zmAlivePlot
+
+
+
+zebra_mussel_DRM_7_A_4 <- drm(pAlive ~ pCO2 , weights = Total,
+                              data = zebra_mussel[ TimeCode == 7 &  Size == "A" & Temp == 4, ] , 
+                              fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_A_4
+
+plot(zebra_mussel_DRM_7_A_4)
+
+ED(zebra_mussel_DRM_7_A_4, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+zebra_mussel_DRM_7_A_20 <- drm(pAlive ~ pCO2 , weights = Total,
+                              data = zebra_mussel[ TimeCode == 7 &  Size == "A" & Temp == 20, ] , 
+                              fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_A_20
+
+plot(zebra_mussel_DRM_7_A_20)
+
+ED(zebra_mussel_DRM_7_A_20, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+
+zebra_mussel_DRM_7_S_4 <- drm(pAlive ~ pCO2 , weights = Total,
+                              data = zebra_mussel[ TimeCode == 7 &  Size == "S" & Temp == 4, ] , 
+                              fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_S_4
+
+plot(zebra_mussel_DRM_7_S_4)
+
+ED(zebra_mussel_DRM_7_S_4, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+zebra_mussel_DRM_7_S_20 <- drm(pAlive ~ pCO2 , weights = Total,
+                              data = zebra_mussel[ TimeCode == 7 &  Size == "S" & Temp == 20, ] , 
+                              fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_S_20
+
+plot(zebra_mussel_DRM_7_S_20)
+
+ED(zebra_mussel_DRM_7_S_20, respLev = c( 50, 1, 99), interval = 'delta')
+
+###########################################################################################################
+###########################################################################################################
+## Analyze Zebra Mussel mortality data
+zebra_mussel[ , pAttached := Attached / Total ]
+
+zmAttachedPlot <-
+    ggplot(data = zebra_mussel[ TimeCode == 7], aes(x = mpCO2, y = pAttached, weight = Total)) +
+    geom_jitter(width = 0, height = 0.05) +
+    facet_grid( Size ~ Temp,
+               labeller = label_bquote(
+                   rows = "Size: "~.(Size),
+                   cols = .(Temp)*degree*"C"),
+               scales = "free_x") +
+    theme_minimal() +
+    xlab(expression("mP"~CO[2])) + 
+    scale_x_continuous(label = comma, trans = 'log10')  +    
+    stat_smooth(method = 'glm', method.args = list(family = 'binomial'))
+zmAttachedPlot
+
+
+zebra_mussel_DRM_7_A_4_attached <- drm(pAttached ~ pCO2 , weights = Total,
+                                      data = zebra_mussel[ TimeCode == 7 &  Size == "A" & Temp == 4, ] , 
+                                      fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_A_4_attached
+plot(zebra_mussel_DRM_7_A_4_attached)
+ED(zebra_mussel_DRM_7_A_4_attached, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+
+zebra_mussel_DRM_7_S_4_attached <- drm(pAttached ~ pCO2 , weights = Total,
+                                      data = zebra_mussel[ TimeCode == 7 &  Size == "S" & Temp == 4, ] , 
+                                      fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_S_4_attached
+plot(zebra_mussel_DRM_7_S_4_attached)
+ED(zebra_mussel_DRM_7_S_4_attached, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+
+zebra_mussel_DRM_7_A_20_attached <- drm(pAttached ~ pCO2 , weights = Total,
+                                      data = zebra_mussel[ TimeCode == 7 &  Size == "A" & Temp == 20, ] , 
+                                      fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_A_20_attached
+plot(zebra_mussel_DRM_7_A_20_attached)
+ED(zebra_mussel_DRM_7_A_20_attached, respLev = c( 50, 1, 99), interval = 'delta')
+
+
+
+zebra_mussel_DRM_7_S_20_attached <- drm(pAttached ~ pCO2 , weights = Total,
+                                      data = zebra_mussel[ TimeCode == 7 &  Size == "S" & Temp == 20, ] , 
+                                      fct = LL.2(), type = 'binomial')
+zebra_mussel_DRM_7_S_20_attached
+plot(zebra_mussel_DRM_7_S_20_attached)
+ED(zebra_mussel_DRM_7_S_20_attached, respLev = c(50, 1, 99), interval = 'delta')
+
+
+
+
 
 
 ### save session info
